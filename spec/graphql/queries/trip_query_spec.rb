e87142380fd17c6fef383aa489900a0fd5b58b45
Denys Husiev <denys.husiev@techmagic.co>
2020-05-10 15:07:10 +0300

    add trip policies

--- spec/graphql/queries/trip_query_spec.rb
+++ spec/graphql/queries/trip_query_spec.rb
@@ -21,9 +21,9 @@ RSpec.describe Queries::TripQuery, type: :request do
   let(:trip) { create(:trip) }
 
   describe '#resolve' do
-    context 'when user is authenticated' do
-      subject(:request) { valid_gql_request(query, user) }
+    subject(:request) { valid_gql_request(query, user) }
 
+    context 'when user is authenticated and has access to trip' do
       let(:user) { trip.creator }
 
       it 'returns correct trip' do
@@ -38,6 +38,36 @@ RSpec.describe Queries::TripQuery, type: :request do
       it 'returns correct creator' do
         request
         expect(json[:data][:trip][:creator][:id]).to eq user.id.to_s
+      end
+    end
+
+    context 'when user does not have access to trip' do
+      let(:user) { create(:trip).creator }
+
+      it 'returns errors' do
+        request
+        expect(json[:errors].first[:extensions][:code])
+          .to eq EasyTripBackEndSchema::AUTHENTICATION_ERROR_CODE
+      end
+    end
+
+    context 'when user is admin' do
+      let(:user) { create(:trip).creator }
+
+      before { user.add_role(:admin) }
+
+      it 'returns correct trip' do
+        request
+        expect(json[:data][:trip]).to include(id: trip.id.to_s,
+                                              name: trip.name,
+                                              description: trip.description,
+                                              startDate: trip.start_date,
+                                              finishDate: trip.finish_date)
+      end
+
+      it 'returns correct creator' do
+        request
+        expect(json[:data][:trip][:creator][:id]).to eq trip.creator.id.to_s
       end
     end
 
