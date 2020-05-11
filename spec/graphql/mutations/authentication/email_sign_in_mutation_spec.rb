0f4d2d355d699bc990b8e09025c6312eb709ccd9
Denys Husiev <denys.husiev@techmagic.co>
2020-05-11 20:36:28 +0300

    add sign in mutation spec

---
+++ spec/graphql/mutations/authentication/email_sign_in_mutation_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+describe Mutations::Authentication::EmailSignInMutation, type: :request do
+  let(:query) do
+    <<~GQL
+      mutation {
+        emailSignIn(email: "#{email}", password: "#{password}") {
+          authToken
+          user {
+            id
+          }
+        }
+      }
+    GQL
+  end
+
+  let!(:user) { create(:user, email_identity_attributes: { email: email, password: password }) }
+  let(:email) { Faker::Internet.email }
+  let(:password) { Faker::Internet.password }
+
+
+  describe '#resolve' do
+    subject(:request) { gql_request(query) }
+
+    it 'calls Authentication::EmailSignIn Service' do
+      expect(::Authentication::EmailSignIn).to receive(:call).with(email, password)
+      request
+    end
+
+    it 'returns authToken' do
+      request
+      expect(json[:data][:emailSignIn][:authToken]).to be_present.and(be_a(String))
+    end
+
+    it 'returns correct user' do
+      request
+      expect(json[:data][:emailSignIn][:user][:id]).to eq user.id.to_s
+    end
+  end
+end
